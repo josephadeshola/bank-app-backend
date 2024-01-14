973d8a063d0b09cd7e2d7188b398e0afd4c3a391
Adeshola Ayomide <josephay125d@gmail.com>
2024-01-14 13:41:27 -0800

    my signup backend

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$localhost = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'bank_db';
+$dbconnection = new mysqli($localhost, $username, $password, $database);
+if ($dbconnection->connect_error) {
+    echo 'Not connected: ' . $dbconnection->connect_error;
+} else {
+    echo 'Connected successfully!';
+}
+?>
