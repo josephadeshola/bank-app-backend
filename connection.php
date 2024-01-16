65c8bd50f8cb5a7e7e3960dc897f462209e13cc6
Adeshola Ayomide <josephay125d@gmail.com>
2024-01-16 14:04:41 -0800

    userdata stored

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 $localhost = 'localhost';
 $username = 'root';
 $password = '';
@@ -6,7 +9,5 @@ $database = 'bank_db';
 $dbconnection = new mysqli($localhost, $username, $password, $database);
 if ($dbconnection->connect_error) {
     echo 'Not connected: ' . $dbconnection->connect_error;
-} else {
-    echo 'Connected successfully!';
 }
 ?>
--- signup.php
+++ signup.php
@@ -1,19 +1,19 @@
 <?php
 require 'connection.php';
-require 'header.php';
 
 $input = json_decode(file_get_contents("php://input"), true);
-$fullName = $input['fullName'] ?? '';
-$email = $input['email'] ?? '';
-$phone = $input['phone'] ?? '';
-$password = $input['password'] ?? '';
-$userName = $input['userName'] ?? '';
-$birth = $input['birth'] ?? '';
-$address = $input['address'] ?? '';
-$nin_bvn = $input['nin_bvn'] ?? '';
-$language = $input['language'] ?? '';
-$marital = $input['marital'] ?? '';
-echo json_encode($marital) ?? '';
+
+$fullName = $input['fullName'];
+$email = $input['email'];
+$phone = $input['phone'];
+$password = $input['password'];
+$userName = $input['userName'];
+$birth = $input['birth'];
+$address = $input['address'];
+$nin_bvn = $input['nin_bvn']; 
+$language = $input['language'];
+$marital = $input['marital'];
+$gender= $input['gender'];
 
 $query = "SELECT * FROM bankinfo WHERE email=?";
 $prepare = $dbconnection->prepare($query);
@@ -22,23 +22,22 @@ $prepare->execute();
 $existingUser = $prepare->get_result()->fetch_assoc();
 
 if ($existingUser) {
-    echo json_encode(array('success' => false, 'message' => 'Email already exists'));
+    echo json_encode(array('status' => false, 'message' => 'Email already exists'));
 } else {
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Insert user data into the database
-    $mysql = "INSERT INTO `bankinfo`(`fullName`, `email`, `phone`, `password`, `userName`, `birth`, `address`, `nin_bvn`, `language`, `marital`) VALUES (?,?,?,?,?,?,?,?,?,?)";
+    $mysql = "INSERT INTO `bankinfo` (`fullName`, `email`, `phone`, `password`, `userName`, `birth`, `address`, `nin_bvn`, `language`, `marital`,`gender`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
     $prepare = $dbconnection->prepare($mysql);
 
-    $prepare->bind_param("ssssssssss", $fullName, $email, $phone, $hashedPassword, $userName, $birth, $address, $nin_bvn, $language, $marital);
-    $execute = $prepare->execute();
+    $prepare->bind_param("sssssssssss", $fullName, $email, $phone, $hashedPassword, $userName, $birth, $address, $nin_bvn, $language,$marital,$gender);
+    $execute= $prepare->execute();
 
     if ($execute) {
-        echo json_encode(array('success' => true, 'message' => 'User registered successfully'));
+        $result =['email'=>$email , 'status' => true, 'message' => 'User registered successfully'];
+        echo json_encode($result);
     } else {
-        echo json_encode(array('success' => false, 'message' => 'Error registering user', 'error' => $prepare->error));
+        $result =['status' => false, 'message' => 'Error registering user'];
+        echo json_encode($result);
     }
 }
-
-
-?> 
+?>
